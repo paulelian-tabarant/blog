27f6c43a728c2a102b70c257ff9bc8e945dfbb9f
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2023-02-18 12:05:07 +0100

    tech: extract posts listing items into PostThumbnail

---
+++ src/components/PostThumbnail.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import { GatsbyImage } from 'gatsby-plugin-image'
+import {
+  posts_listing__item,
+  posts_listing__item__header,
+  posts_listing__item__header__title,
+  posts_listing__item__header__date,
+  posts_listing__item__cover,
+  posts_listing__item__excerpt,
+} from './posts_listing.module.css'
+import { Post } from './posts.type'
+
+interface PostThumbnailProps {
+  post: Post
+}
+
+const PostThumbnail: React.FC<PostThumbnailProps> = ({ post }) => {
+  const { path: postPath, title, date, featuredImage } = post.frontmatter
+
+  const imageData = featuredImage.childImageSharp.gatsbyImageData
+
+  return (
+    <a key={postPath} href={postPath}>
+      <article className={posts_listing__item} key={postPath}>
+        {imageData && (
+          <GatsbyImage
+            alt=""
+            className={posts_listing__item__cover}
+            image={imageData}
+          />
+        )}
+        <header className={posts_listing__item__header}>
+          <h2 className={posts_listing__item__header__title}>{title}</h2>
+          <div className={posts_listing__item__header__date}>{date}</div>
+        </header>
+        <p className={posts_listing__item__excerpt}>{post.excerpt}</p>
+      </article>
+    </a>
+  )
+}
+
+export default PostThumbnail
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,43 +1,10 @@
 import * as React from 'react'
-import { GatsbyImage } from 'gatsby-plugin-image'
-import {
-  posts_listing,
-  posts_listing__item,
-  posts_listing__item__header,
-  posts_listing__item__header__title,
-  posts_listing__item__header__date,
-  posts_listing__item__cover,
-  posts_listing__item__excerpt,
-} from './posts_listing.module.css'
-
+import { posts_listing } from './posts_listing.module.css'
 import { PostsProps, Post } from './posts.type'
+import PostThumbnail from './PostThumbnail'
 
-function createThumbnail(post: Post) {
-  const { path: postPath, title, date, featuredImage } = post.frontmatter
-
-  const imageData = featuredImage.childImageSharp.gatsbyImageData
-
-  return (
-    <a key={postPath} href={postPath}>
-      <article className={posts_listing__item} key={postPath}>
-        {imageData && (
-          <GatsbyImage
-            alt=""
-            className={posts_listing__item__cover}
-            image={imageData}
-          />
-        )}
-        <header className={posts_listing__item__header}>
-          <h2 className={posts_listing__item__header__title}>{title}</h2>
-          <div className={posts_listing__item__header__date}>{date}</div>
-        </header>
-        <p className={posts_listing__item__excerpt}>{post.excerpt}</p>
-      </article>
-    </a>
-  )
-}
-
-const listPosts = (posts: Array<Post>) => posts.map(createThumbnail)
+const listPosts = (posts: Array<Post>) =>
+  posts.map((post) => <PostThumbnail post={post}></PostThumbnail>)
 
 const Posts: React.FC<PostsProps> = (props) => {
   const { title, path, posts } = props
